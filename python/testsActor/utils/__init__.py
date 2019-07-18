be9b121af12554c40cca55d9a530d34e34ca6bc7
Arnaud Le Fur <arnaud.lefur@lam.fr>
2019-07-18 16:39:00 +0200

    enuActor no longer needed

---
+++ python/testsActor/utils/__init__.py
@@ -0,0 +1,49 @@
+import socket
+import time
+from functools import partial
+
+from actorcore.QThread import QThread
+
+
+def connectSock(host, port, timeout=1):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(timeout)
+        s.connect((host, port))
+        s.close()
+    except:
+        time.sleep(1)
+        return False
+
+    return True
+
+
+def waitForTcpServer(host, port, timeout=60):
+    start = time.time()
+    port = int(port)
+    while not connectSock(host, port):
+        if time.time() - start > timeout:
+            raise TimeoutError('tcp server %s:%d is not running' % (host, port))
+
+    return True
+
+
+def putMsg(func):
+    def wrapper(self, cmd, *args, **kwargs):
+        thr = QThread(self.actor, str(time.time()))
+        thr.start()
+        thr.putMsg(partial(func, self, cmd, *args, **kwargs))
+        thr.exitASAP = True
+
+    return wrapper
+
+
+def singleShot(func):
+    @putMsg
+    def wrapper(self, cmd, *args, **kwargs):
+        try:
+            return func(self, cmd, *args, **kwargs)
+        except Exception as e:
+            cmd.fail('text=%s' % self.actor.strTraceback(e))
+
+    return wrapper
