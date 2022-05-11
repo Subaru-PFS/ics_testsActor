32520e566104c48d80d589344b9ec0a6a3d42de8
Arnaud Le Fur <afur@princeton.astro.edu>
2022-05-10 18:35:21 -1000

    genConvergenceVisit for convergencePlot testing purpose

--- python/testsActor/Commands/TopCmd.py
+++ python/testsActor/Commands/TopCmd.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
 from builtins import object
+
 import opscore.protocols.keys as keys
 import opscore.protocols.types as types
-from opscore.utility.qstr import qstr
+
 
 class TopCmd(object):
 
@@ -21,26 +22,26 @@ class TopCmd(object):
             ('status', '', self.status),
             ('connect', '<controller> [<name>]', self.connect),
             ('disconnect', '<controller>', self.disconnect),
-         ]
+            ('genConvergenceVisit', '<visit>', self.genConvergenceVisit),
+        ]
 
         # Define typed command arguments for the above commands.
         self.keys = keys.KeysDictionary("xcu_xcu", (1, 1),
                                         keys.Key("name", types.String(),
                                                  help='an optional name to assign to a controller instance'),
-                                        keys.Key("controllers", types.String()*(1,None),
+                                        keys.Key("controllers", types.String() * (1, None),
                                                  help='the names of 1 or more controllers to load'),
                                         keys.Key("controller", types.String(),
                                                  help='the names a controller.'),
+                                        keys.Key("visit", types.Int(), )
                                         )
-
-
 
     def controllerKey(self):
         controllerNames = list(self.actor.controllers.keys())
         key = 'controllers=%s' % (','.join([c for c in controllerNames]))
 
         return key
-    
+
     def connect(self, cmd, doFinish=True):
         """ Reload all controller objects. """
 
@@ -54,12 +55,12 @@ class TopCmd(object):
             self.actor.attachController(controller,
                                         instanceName=instanceName)
         except Exception as e:
-                cmd.fail('text="failed to connect controller %s: %s"' % (instanceName,
-                                                                         e))
-                return
+            cmd.fail('text="failed to connect controller %s: %s"' % (instanceName,
+                                                                     e))
+            return
 
         cmd.finish(self.controllerKey())
-        
+
     def disconnect(self, cmd, doFinish=True):
         """ Disconnect the given, or all, controller objects. """
 
@@ -82,7 +83,7 @@ class TopCmd(object):
         """Report camera status and actor version. """
 
         self.actor.sendVersionKey(cmd)
-        
+
         cmd.inform('text=%s' % ("Present!"))
         cmd.inform('text="config id=0x%08x %r"' % (id(self.actor.config),
                                                    self.actor.config.sections()))
@@ -90,6 +91,10 @@ class TopCmd(object):
         if 'all' in cmd.cmd.keywords:
             for c in self.actor.controllers:
                 self.actor.callCommand("%s status" % (c))
-            
+
         cmd.finish(self.controllerKey())
 
+    def genConvergenceVisit(self, cmd):
+        cmdKeys = cmd.cmd.keywords
+        visitId = cmdKeys['visit'].values[0]
+        cmd.finish(f'convergenceId={visitId}')
